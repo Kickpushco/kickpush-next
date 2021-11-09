e6b3e3b283e2df3e7438b9553051b38352e9fb0d
Rob Sterlini <github@robsterlini.co.uk>
2021-11-09 17:54:34 +0000

    fix: Add page grab to 404 page to get build working again
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,19 +1,20 @@
-import Paragraph from "components/Paragraph/Paragraph";
 import clsx from "clsx";
+
+import Paragraph from "components/Paragraph/Paragraph";
 
 import styles from "./Hero.module.scss";
 
-function Hero({ children }) {
+function Hero({ className, children }) {
   return (
-    <section className={styles.Hero}>
+    <section className={clsx(className, styles.Hero)}>
       <div className={clsx("container", styles.Container)}>{children}</div>
     </section>
   );
 }
 
-export function HeroCopy({ children }) {
+export function HeroCopy({ className, children }) {
   return (
-    <Paragraph className={styles.Copy} level="huge">
+    <Paragraph className={clsx(className, styles.Copy)} level="huge">
       {children}
     </Paragraph>
   );
--- pages/404.js
+++ pages/404.js
@@ -1,23 +1,37 @@
 import Head from "next/head";
 
-import Nav from "components/Nav/Nav";
+import { fetchCustomPage } from "services/contentful";
+
+import { ContentfulNav } from "components/Nav/Nav";
 import Hero from "components/Hero/Hero";
 import Heading from "components/Heading/Heading";
 import Title from "components/Meta/Title";
 
-export default function PageNotFound() {
+import styles from "sass/pages/404.module.scss";
+
+export default function PageNotFound({ globalSettings }) {
   return (
     <>
+      {/* TODO: Add to CMS */}
       <Title shortTitle="Page not found" />
       <Head>
         <meta name="robots" content="noindex" />
       </Head>
 
-      <Nav />
+      <ContentfulNav globalSettings={globalSettings} />
 
-      <Hero>
+      <Hero className={styles.Hero}>
+        {/* TODO: Add to CMS */}
         <Heading level="h1">Looks like you got lost.</Heading>
       </Hero>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const props = await fetchCustomPage("customPageHome", { include: 1 });
+
+  return {
+    props,
+  };
 }
