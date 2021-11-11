221dbed7268c41a461e2f889936310db439da457
Rob Sterlini <github@robsterlini.co.uk>
2021-11-11 17:20:15 +0000

    feat: Add _document for lang, move fonts to public and preload
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,33 @@ import { AppContextProvider } from "context/state";
 
 import "../sass/pages/_app.scss";
 
+const PRELOAD_FONTS = [
+  "kickpush-headings",
+  "lineto-circular-bold",
+  "lineto-circular-medium",
+];
+const FAVICON_SIZES = [16, 32, 96];
+
 function App({ Component, pageProps }) {
   return (
     <AppContextProvider>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        {FAVICON_SIZES.map((iconSize, iconSizeIndex) => (
+          <link
+            key={`icon-${iconSizeIndex}`}
+            href={`/favicon-${iconSize}x${iconSize}.png`}
+            rel="icon"
+            type="image/png"
+          />
+        ))}
+        {PRELOAD_FONTS.map((font, fontIndex) => (
+          <link
+            key={`font-${fontIndex}`}
+            rel="preload"
+            href={`/fonts/${font}.woff2`}
+            as="font"
+          />
+        ))}
       </Head>
 
       <Component {...pageProps} />
