a6a15abf618cfb0f0fff1c358386e84c456d17f8
Rob Sterlini <github@robsterlini.co.uk>
2021-11-11 18:11:34 +0000

    feat: Add other icons
    Signed-off-by: Rob Sterlini <github@robsterlini.co.uk>

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import Head from "next/head";
 
 import { AppContextProvider } from "context/state";
 
-import "../sass/pages/_app.scss";
+import "sass/pages/_app.scss";
+import styles from "sass/pages/_app.module.scss";
 
 const PRELOAD_FONTS = [
   "kickpush-headings",
@@ -10,19 +11,37 @@ const PRELOAD_FONTS = [
   "lineto-circular-medium",
 ];
 const FAVICON_SIZES = [16, 32, 96];
+const APPLE_ICON_SIZES = [57, 60, 72, 76, 114, 120, 144, 152, 180, 192];
+const ANDROID_ICON_SIZE = 192;
 
 function App({ Component, pageProps }) {
   return (
     <AppContextProvider>
       <Head>
-        {FAVICON_SIZES.map((iconSize, iconSizeIndex) => (
+        {FAVICON_SIZES.map((size) => (
           <link
-            key={`icon-${iconSizeIndex}`}
-            href={`/favicon-${iconSize}x${iconSize}.png`}
+            key={`favicon-${size}`}
+            href={`/favicon-${size}.png`}
             rel="icon"
+            sizes={`${size}x${size}`}
             type="image/png"
           />
         ))}
+        {APPLE_ICON_SIZES.map((size) => (
+          <link
+            key={`apple-icon-${size}`}
+            href={`/logo-${size}.png`}
+            rel="apple-touch-icon"
+            sizes={`${size}x${size}`}
+          />
+        ))}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes={`${ANDROID_ICON_SIZE}x${ANDROID_ICON_SIZE}`}
+          href={`/logo-${ANDROID_ICON_SIZE}.png`}
+        />
+
         {PRELOAD_FONTS.map((font, fontIndex) => (
           <link
             key={`font-${fontIndex}`}
@@ -31,6 +50,10 @@ function App({ Component, pageProps }) {
             as="font"
           />
         ))}
+
+        <meta name="msapplication-TileColor" content={styles.kickpushRed} />
+        <meta name="msapplication-TileImage" content="/icon-144.png" />
+        <meta name="theme-color" content={styles.kickpushRed} />
       </Head>
 
       <Component {...pageProps} />
